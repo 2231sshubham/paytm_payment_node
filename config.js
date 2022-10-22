f466cffe93934f58a997da8378e63a6039546254
2231sshubham <2231sshubham@gmail.com>
2022-10-22 14:15:53 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+
+var MID = "lmwbur84273590020588";
+var MKEY = "#gIzimKR3jGmdyoG";
+
+var ENV= 'securegw-stage.paytm.in';
+var WEBSITE= 'WEBSTAGING';
+
+exports.MID = MID;
+exports.MKEY = MKEY;
+exports.ENV = ENV;
+exports.WEBSITE = WEBSITE;
